f4c1c5711d217700498cd641fa0000c51be9f277
HUNGNV10-D5\HungNV10 <hung.nv155790hust@gmail.com>
2024-07-10 10:54:10 +0700

    build: docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  build: {
+    outDir: "./docs",
+  },
 });
